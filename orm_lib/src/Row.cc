85739fc37c0044dd14eb6a9e70902f0775e702f2
antao <antao2002@gmail.com>
2018-10-18 16:18:40 +0800

    .

--- orm_lib/src/Row.cc
+++ orm_lib/src/Row.cc
@@ -2,12 +2,11 @@
 #include <drogon/orm/Field.h>
 #include <drogon/orm/RowIterator.h>
 using namespace drogon::orm;
-Row::Row(const Result &r,size_type index) noexcept :
-_result(r),
-_index(long(index)),
-_end(r.columns())
+Row::Row(const Result &r, size_type index) noexcept
+    : _result(r),
+      _index(long(index)),
+      _end(r.columns())
 {
-
 }
 Row::size_type Row::size() const
 {
@@ -15,32 +14,37 @@ Row::size_type Row::size() const
 }
 Row::reference Row::operator[](size_type index) const
 {
-    if(index>=_end)
-        throw;//TODO throw....
-    return Field(*this,index);
+    if (index >= _end)
+        throw; //TODO throw....
+    return Field(*this, index);
 }
 
 Row::reference Row::operator[](const char columnName[]) const
 {
-    auto N=_result.columnNumber(columnName);
-    return Field(*this,N);
+    auto N = _result.columnNumber(columnName);
+    return Field(*this, N);
 }
 
-Row::reference Row::operator[](const std::string &columnName) const{
+Row::reference Row::operator[](const std::string &columnName) const
+{
     return operator[](columnName.c_str());
 }
 
-Row::const_iterator Row::begin() const noexcept {
-    return const_iterator(*this,0);
+Row::const_iterator Row::begin() const noexcept
+{
+    return const_iterator(*this, 0);
 }
-Row::const_iterator Row::cbegin() const noexcept {
+Row::const_iterator Row::cbegin() const noexcept
+{
     return begin();
 }
-Row::const_iterator Row::end() const noexcept {
-    return const_iterator(*this,size());
+Row::const_iterator Row::end() const noexcept
+{
+    return const_iterator(*this, size());
 }
 
-Row::const_iterator Row::cend() const noexcept {
+Row::const_iterator Row::cend() const noexcept
+{
     return end();
 }
 
@@ -66,23 +70,27 @@ Row::const_iterator Row::const_reverse_iterator::base() const noexcept
     return ++tmp;
 }
 
-const_row_iterator const_row_iterator::operator++(int) {
+const_row_iterator const_row_iterator::operator++(int)
+{
     const_row_iterator old(*this);
     _column++;
     return old;
 }
-const_row_iterator const_row_iterator::operator--(int) {
+const_row_iterator const_row_iterator::operator--(int)
+{
     const_row_iterator old(*this);
     _column--;
     return old;
 }
 
-const_reverse_row_iterator const_reverse_row_iterator::operator++(int) {
+const_reverse_row_iterator const_reverse_row_iterator::operator++(int)
+{
     const_reverse_row_iterator old(*this);
     iterator_type::operator--();
     return old;
 }
-const_reverse_row_iterator const_reverse_row_iterator::operator--(int) {
+const_reverse_row_iterator const_reverse_row_iterator::operator--(int)
+{
     const_reverse_row_iterator old(*this);
     iterator_type::operator++();
     return old;
