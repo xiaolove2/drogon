6975130c2f772e027222d78d4c4a25d387e3c443
antao <antao2002@gmail.com>
2018-10-18 16:27:43 +0800

    Add comment

---
+++ orm_lib/inc/drogon/orm/Mapper.h
@@ -0,0 +1,27 @@
+/**
+ *
+ *  Mapper.h
+ *  An Tao
+ *
+ *  Copyright 2018, An Tao.  All rights reserved.
+ *  Use of this source code is governed by a MIT license
+ *  that can be found in the License file.
+ *
+ *  @section DESCRIPTION
+ *
+ */
+
+#pragma once
+namespace drogon
+{
+namespace orm
+{
+
+template <typename T>
+class Mapper
+{
+
+};
+
+} // namespace orm
+} // namespace drogon
--- orm_lib/inc/drogon/orm/Row.h
+++ orm_lib/inc/drogon/orm/Row.h
@@ -1,3 +1,16 @@
+/**
+ *
+ *  Row.h
+ *  An Tao
+ *
+ *  Copyright 2018, An Tao.  All rights reserved.
+ *  Use of this source code is governed by a MIT license
+ *  that can be found in the License file.
+ *
+ *  @section DESCRIPTION
+ *
+ */
+
 #pragma once
 
 #include <drogon/orm/Result.h>
